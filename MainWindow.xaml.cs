2cd87e9e9d5096810c36961663cd4d5857fbec21
mabyre <mabyre@sodevlog.com>
2022-04-22 16:42:47 +0200

    add DependencyProperty Sample

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,23 +20,35 @@ namespace WpfAppCore1
     /// </summary>
     public partial class MainWindow : Window
     {
-        UserControl1 ucc = null;
-
         public MainWindow()
         {
             InitializeComponent();
+
+            // Simulate a clic on button to load UserControl
+            Button b = new Button();
+            b.Name = "buttonTest2";
+            button_Click( b, null );
         }
 
-        private void buttonTest1_Click( object sender, RoutedEventArgs e )
+        private void button_Click( object sender, RoutedEventArgs e )
         {
             double uccWidth = MyMainWindow.Width;
             double uccHeight = MyMainWindow.Height;
 
             if ( (sender as Button).Name == "buttonTest1" )
             {
-                ucc = new UserControl1();
+                UserControl1 ucc = new UserControl1();
                 uccWidth = Column0.Width.Value + ucc.Width;
                 uccHeight = ucc.Height;
+                ContentControlArea.Content = ucc;
+            }
+
+            if ( (sender as Button).Name == "buttonTest2" )
+            {
+                UserControl2 ucc = new UserControl2();
+                uccWidth = Column0.Width.Value + ucc.Width;
+                uccHeight = ucc.Height;
+                ucc.SetText = "Hello World!"; // DependencyProperty
                 ContentControlArea.Content = ucc;
             }
 
---
+++ UserControl2.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppCore1
+{
+    /// <summary>
+    /// Logique d'interaction pour UserControl2.xaml
+    /// </summary>
+    public partial class UserControl2 : UserControl
+    {
+        public UserControl2()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty SetTextProperty =
+         DependencyProperty.Register( "SetText", typeof( string ), typeof( UserControl2 ), new
+            PropertyMetadata( "", new PropertyChangedCallback( OnSetTextChanged ) ) );
+
+        public string SetText
+        {
+            get { return (string)GetValue( SetTextProperty ); }
+            set { SetValue( SetTextProperty, value ); }
+        }
+
+        private static void OnSetTextChanged( DependencyObject d, DependencyPropertyChangedEventArgs e )
+        {
+            UserControl2 uc = d as UserControl2;
+            uc.OnSetTextChanged( e );
+        }
+
+        private void OnSetTextChanged( DependencyPropertyChangedEventArgs e )
+        {
+            TextBox1.Text = e.NewValue.ToString();
+        }
+
+        private void Button_Click( object sender, RoutedEventArgs e )
+        {
+            this.SetText = "Coucou world!";
+        }
+    }
+}
